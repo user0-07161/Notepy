bf0b202ad65249238a1b98a952a026a0282a32ce
User0 <you@example.com>
2024-07-08 04:36:47 -0400

    huge update, translatable using po files

--- installer.bat
+++ installer.bat
@@ -5,10 +5,8 @@ echo If you want to proceed with installation, press any key. If you close this 
 pause > nul
 echo Installing...
 mkdir "%userprofile%\Notepy"
-copy notepy_en "%userprofile%\Notepy"
-copy notepy_pl "%userprofile%\Notepy"
-copy notepy_en\src "%userprofile%\Notepy"
-copy notepy_pl\src "%userprofile%\Notepy"
+copy %~dp0 "%userprofile%\Notepy"
+copy %~dp0\src "%userprofile%\Notepy"
 echo Error code (0 means everything is okay, if it isn't, use "certutil /error %errorlevel%"): %errorlevel%
 echo Press any key to exit...
 pause > nul
